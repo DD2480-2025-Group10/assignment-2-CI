fe54c237ff81e9908c60aa5dbd8d0ad9014f9a55
Alrik Jonek Holmquist <alrik.jonek@altostruct.se>
2026-02-09 16:30:54 +0100

    ci: test webhook

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    print("yoyoyfod")
+    print("yoyoyfasdod")
 
     if not data or "repository" not in data:
         return jsonify({"error": "Invalid payload"}), 400
